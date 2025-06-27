1fef1add7fe916dcdb782ee53701bfb457686032
zakochan <notbadapple@gmail.com>
2025-06-28 07:20:35 +0800

    蟑螂 家具 玩家类

---
+++ Game/Script/GameObject/Periplaneta.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public enum Periplaneta_Status
+{
+	FREE,
+	POSSESS,
+}
+
+
+public partial class Periplaneta : Node2D
+{
+	[Export] public Periplaneta_Status Status;
+	[Export] public float CheckRound;
+	[Export] public float CD; // todo: global cd
+	[Export] public float Speed;
+
+	[Signal] public delegate void KillEventHandler();
+
+	private Furniture _possessFurniture;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if (Status == Periplaneta_Status.FREE)
+		{
+			FreeMove();
+		}
+		else if (Status == Periplaneta_Status.POSSESS)
+		{
+			PossessMove();
+		}
+	}
+
+	public void FreeMove()
+	{
+		// todo
+	}
+
+	public void PossessMove()
+	{
+		// todo
+		_possessFurniture.EmitSignal(Furniture.SignalName.Move);
+	}
+
+}
---
+++ Game/Script/GameObject/Player.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+
+public enum PlayerID
+{
+	Player1,
+	Player2,
+}
+
+public partial class Player : CharacterBody2D
+{
+	[Export] public PlayerID PlayerID;
+
+	public FlipFlop flipFlop;
+
+	private Furniture _faceFurniture;
+	private Furniture _holdupFurniture;
+
+
+	public override void _Ready()
+	{
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		// todo: move
+		// todo: interactive
+		// todo: holdup
+		// todo: attack
+	}
+
+	public void Interactive()
+	{
+		_faceFurniture.EmitSignal(Furniture.SignalName.Interactive);
+	}
+
+	public void Holdup()
+	{
+		_faceFurniture.EmitSignal(Furniture.SignalName.Holdup);
+	}
+
+	public void FreeMove()
+	{
+		// todo
+	}
+
+	public void HoldupMove()
+	{
+		// todo
+		_holdupFurniture.EmitSignal(Furniture.SignalName.Move);
+	}
+}
