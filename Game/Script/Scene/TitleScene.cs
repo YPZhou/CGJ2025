1f0280e87e7c862f74d7ccab5ffac19cead69a6d
YPZhou <yunpeng_zhou@163.com>
2025-06-28 07:34:33 +0800

    完成标题场景交互

--- Game/Script/Scene/TitleScene.cs
+++ Game/Script/Scene/TitleScene.cs
@@ -4,20 +4,60 @@ namespace CGJ2025;
 
 public partial class TitleScene : Node
 {
+	public override void _Ready()
+	{
+		base._Ready();
+
+		currentSelection = 0;
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+
+		switch (currentSelection)
+		{
+			case 0:
+				startRect.Scale = new Vector2(1.1f, 1.1f);
+				quitRect.Scale = new Vector2(1.0f, 1.0f);
+				break;
+			case 1:
+				startRect.Scale = new Vector2(1.0f, 1.0f);
+				quitRect.Scale = new Vector2(1.1f, 1.1f);
+				break;
+		}
+	}
+
 	public override void _Input(InputEvent @event)
 	{
 		base._Input(@event);
 
 		if (@event is InputEventKey keyEvent && keyEvent.IsPressed())
 		{
-			if (keyEvent.Keycode == Key.Escape)
+			if (keyEvent.Keycode == Key.Enter || keyEvent.Keycode == Key.Space)
 			{
-				GetTree().Quit();
+				if (currentSelection == 0)
+				{
+					GetTree().ChangeSceneToFile("res://Scene/Main.tscn");
+				}
+				else if (currentSelection == 1)
+				{
+					GetTree().Quit();
+				}
 			}
-			else if (keyEvent.Keycode == Key.Enter || keyEvent.Keycode == Key.Space)
+			else if (keyEvent.Keycode == Key.W || keyEvent.Keycode == Key.Up)
 			{
-				GetTree().ChangeSceneToFile("res://Scene/Main.tscn");
+				currentSelection = (currentSelection - 1 + 2) % 2; // Wrap around
+			}
+			else if (keyEvent.Keycode == Key.S || keyEvent.Keycode == Key.Down)
+			{
+				currentSelection = (currentSelection + 1) % 2; // Wrap around
 			}
 		}
 	}
+
+	int currentSelection;
+
+	[Export] TextureRect startRect;
+	[Export] TextureRect quitRect;
 }
