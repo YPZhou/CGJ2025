851b0f49e17f29e88497fb12faf6bc0622d31fd3
YPZhou <yunpeng_zhou@163.com>
2025-06-28 07:00:50 +0800

    从标题场景切换至主场景

--- Game/Script/Scene/TitleScene.cs
+++ Game/Script/Scene/TitleScene.cs
@@ -4,4 +4,20 @@ namespace CGJ2025;
 
 public partial class TitleScene : Node
 {
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
+
+		if (@event is InputEventKey keyEvent && keyEvent.IsPressed())
+		{
+			if (keyEvent.Keycode == Key.Escape)
+			{
+				GetTree().Quit();
+			}
+			else if (keyEvent.Keycode == Key.Enter || keyEvent.Keycode == Key.Space)
+			{
+				GetTree().ChangeSceneToFile("res://Scene/Main.tscn");
+			}
+		}
+	}
 }
